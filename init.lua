1bc762f989bb49cbba4c9e1315da336ba0e2ed42
Sean Rawson <sean.e.rawson@gmail.com>
2025-09-28 17:11:34 -0400

    Neovim config updates

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('n', '<F1>', ':split<CR><C-w>5+<C-w><C-w>:terminal<CR>:Neotree<CR>')
+vim.keymap.set('n', '<F1>', ':bp<CR>')
+vim.keymap.set('n', '<F2>', ':bn<CR>')
 vim.keymap.set('n', 'do', ':lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'gd', ':lua require("goto-preview").goto_preview_definition()<CR>')
+vim.keymap.set('n', 'gr', ':lua require("goto-preview").goto_preview_references()<CR>')
+vim.keymap.set('n', 'gP', ':lua require("goto-preview").close_all_win()<CR>')
+--vim.keymap.set('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
 vim.g.loaded_matchparen = false
 vim.opt.number = true
 vim.opt.shiftwidth = 4
@@ -22,12 +27,39 @@ vim.diagnostic.config({
     }
 })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank({timeout=250})
+    end,
+})
+
 require('config.lazy')
-require('mason').setup()
-require('mason-lspconfig').setup {
-    ensure_installed = { 'lua_ls', 'basedpyright' },
-    automatic_installation = true
-}
+
+require('goto-preview').setup({
+  width = 120, -- Width of the floating window
+  height = 15, -- Height of the floating window
+  border = {"↖", "─" ,"┐", "│", "┘", "─", "└", "│"}, -- Border characters of the floating window
+  default_mappings = false, -- Bind default mappings
+  debug = false, -- Print debug information
+  opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+  resizing_mappings = false, -- Binds arrow keys to resizing the floating window.
+  post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  post_close_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  references = { -- Configure the telescope UI for slowing the references cycling window.
+    provider = "telescope", -- telescope|fzf_lua|snacks|mini_pick|default
+    telescope = require("telescope.themes").get_dropdown({ hide_preview = false })
+  },
+  -- These two configs can also be passed down to the goto-preview definition and implementation calls for one off "peak" functionality.
+  focus_on_open = true, -- Focus the floating window when opening it.
+  dismiss_on_move = false, -- Dismiss the floating window when moving the cursor.
+  force_close = true, -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
+  bufhidden = "wipe", -- the bufhidden option to set on the floating window. See :h bufhidden
+  stack_floating_preview_windows = true, -- Whether to nest floating windows
+  same_file_float_preview = true, -- Whether to open a new floating window for a reference within the current file
+  preview_window_title = { enable = true, position = "left" }, -- Whether to set the preview window title as the filename
+  zindex = 1, -- Starting zindex for the stack of floating windows
+  vim_ui_input = true, -- Whether to override vim.ui.input with a goto-preview floating window
+})
 
 local cmp = require('cmp')
 cmp.setup({
@@ -69,5 +101,8 @@ require('lspconfig').basedpyright.setup({
     }
 })
 require('lspconfig').lua_ls.setup({
+    capabilities = capabilities
+})
+require('lspconfig').gopls.setup({
     capabilities = capabilities
 })
--- plugins.lua
+++ plugins.lua
@@ -1,14 +1,36 @@
 return {
     {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig",
-        "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lsp-signature-help",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
+        "mbbill/undotree",
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = { 'lua_ls', 'basedpyright' },
+            automatic_installation = true,
+            automatic_start = false,
+            automatic_enable = false,
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    },
+    {
+        "rmagatti/goto-preview",
+        dependencies = { "rmagatti/logger.nvim" },
+        event = "BufEnter",
+        config = true,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
     },
     {
         "folke/which-key.nvim",
@@ -19,12 +41,23 @@ return {
         end,
     },
     {
+        "pmizio/typescript-tools.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "neovim/nvim-lspconfig"
+        },
+        opts = {}
+    },
+    {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
             "MunifTanjim/nui.nvim"
-        }
+        },
+        lazy = false,
+        ---@module "neo-tree"
+        ---@type neotree.Config?
     }
 }
